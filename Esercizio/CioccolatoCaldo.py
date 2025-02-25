08bb13e2705ed63f09f1e670f9a2f33c8267b242
Paolod01100110 <paolod04@icloud.com>
2025-02-25 15:29:26 +0100

    Update CioccolatoCaldo.py

---
+++ Esercizio/CioccolatoCaldo.py
@@ -0,0 +1,10 @@
+from Cioccolato import Cioccolato
+class CioccolatoCaldo (Cioccolato):
+    def __init__(self, tipo: str, percentuale_cacao: int, temperatura: int, densita: int): #tioi di dati
+        super().__init__(tipo, percentuale_cacao)
+        self.temperatura = temperatura
+        self.densita = densita
+        
+    def produce(self): #metodo di istanza
+        super(). produce()
+        print(f"Temperatura: {self.temperatura}gradi, Densità: {self.densita}")
